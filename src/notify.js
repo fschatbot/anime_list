95e2c314c02b557964c7891f18fccc268bd5a849
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-21 19:42:23 +0530

    Added Notification

--- src/notify.js
+++ src/notify.js
@@ -1,5 +1,29 @@
+import toast from "react-hot-toast";
+
 const SendNotification = (message, type = "info") => {
-	return 1;
+	console.log(`${type}: ${message}`);
+	toast[type === "info" ? "success" : "error"](message, {
+		position: "top-right",
+		autoClose: 3000,
+		closeOnClick: true,
+		pauseOnHover: true,
+		draggable: true,
+	});
+};
+
+const IsURL = (str) => {
+	var pattern = new RegExp(
+		"^(https?:\\/\\/)?" + // protocol
+			"(?:\\/\\/)" + // host may be surrounded by slashes
+			"([\\da-z\\.-]+)\\." + // domain name
+			"([a-z\\.]{2,6})\\/" + // TLD
+			"([\\w\\/\\.\\-\\?\\,]*)?" + // path
+			"([\\w\\?\\=\\&\\-\\+\\%\\$]*)?" + // query string
+			"(#[\\w\\-]*)?$",
+		"i"
+	);
+	return pattern.test(str);
 };
 
 export default SendNotification;
+export { IsURL };
